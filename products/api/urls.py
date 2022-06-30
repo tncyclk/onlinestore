4e47fdd30d808a8e46c19e60b7f42ce8a65ac52b
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2022-06-30 15:08:49 +0300

    export pdf added

--- products/api/urls.py
+++ products/api/urls.py
@@ -4,7 +4,9 @@ from .views import (ManufacturersView, ProductsView,
                     UpdateManufacturerRetrieveView, 
                     DetailManufacturerRetrieveView,
                     CreateManufacturersView, 
-                    DeleteManufacturersView)
+                    DeleteManufacturersView,
+                    products_exportpdf,
+                    manufacturers_exportpdf)
 
 
 urlpatterns = [
@@ -12,12 +14,15 @@ urlpatterns = [
 
     path("products/", ProductsView.as_view(), name="product-list"),
     # path("products/<int:pk>/", product_detail, name="product-detail"),
+    path('products/exportpdf/', products_exportpdf, name='export-pdf'),
 
     path("manufacturers/", ManufacturerRetrieveView.as_view(), name="manufacturer-list"),
     path("manufacturers/create", CreateManufacturersView.as_view(), name="manufacturer-create"),
     path("manufacturers/<int:pk>/edit/", UpdateManufacturerRetrieveView.as_view(), name="manufacturer-edit"),
     path("manufacturers/<int:pk>/", DetailManufacturerRetrieveView.as_view(), name="manufacturer-detail"),
     path("manufacturers/<int:pk>/delete/", DeleteManufacturersView.as_view(), name="manufacturer-delete"),
+    path('manufacturers/exportpdf/', manufacturers_exportpdf, name='export-pdf'),
+
 
    
 ]
--- products/api/views.py
+++ products/api/views.py
@@ -1,6 +1,14 @@
+from itertools import product
 from .serializers import ManufacturerSerializer, ProductsSerializer, ManufacturerRetrieveSerializer
 from products.models import Product, Manufacturer
 from rest_framework.generics import ListAPIView, RetrieveUpdateAPIView, RetrieveAPIView, CreateAPIView, DestroyAPIView
+
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+import tempfile
+from weasyprint import HTML
+from django.template.loader import render_to_string
+import datetime
 
 
 class ProductsView(ListAPIView):
@@ -32,3 +40,46 @@ class DetailManufacturerRetrieveView(RetrieveAPIView):
 class DeleteManufacturersView(DestroyAPIView):
     queryset = Manufacturer.objects.all()
     serializer_class = ManufacturerSerializer
+
+
+@api_view(['GET'])
+def products_exportpdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = ' attachment; filename=Products' + \
+                                      str(datetime.datetime.now()) + '.pdf'
+    
+    response['Content-Transfer-Encoding'] = 'binary'
+    products = Product.objects.all()
+    html_string = render_to_string('products/product.html', {'products': products})
+    html = HTML(string=html_string)
+    result = html.write_pdf()
+    
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        
+        output = open(output.name, 'rb')
+        response.write(output.read())
+        
+    return response
+
+@api_view(['GET'])
+def manufacturers_exportpdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = ' attachment; filename=Manufacturer' + \
+                                      str(datetime.datetime.now()) + '.pdf'
+    
+    response['Content-Transfer-Encoding'] = 'binary'
+    manufacturers = Manufacturer.objects.all()
+    html_string = render_to_string('manufacturers/manufacturer.html', {'manufacturers': manufacturers})
+    html = HTML(string=html_string)
+    result = html.write_pdf()
+    
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        
+        output = open(output.name, 'rb')
+        response.write(output.read())
+        
+    return response
